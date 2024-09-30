287110b3376d82b7e88c6c0b7e74d8df47a91aca
Azam Fabien <azam.fabien@gmail.com>
2024-09-30 18:19:56 +0200

    project finished v1

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,14 +1,36 @@
 import { useState } from "react";
 import * as S from "./Slider-styled";
 import "./Slider.css";
+import { useMemo } from "react";
 
 function Slider({ toggleTheme }: { toggleTheme: () => void }) {
   const [isMoved, setIsMoved] = useState<boolean>(false);
+  const [count, setCount] = useState<number>(0);
+  const [starPositions, setStarPositions] = useState<
+  { widthHeight: string; top: string; left: string }[]
+>([]);
 
   const handleClick = () => {
     toggleTheme();
     setIsMoved(!isMoved);
+    setCount(count + 1);
   };
+
+  useMemo(() => {
+    if (count % 2 !== 0) {
+      const newStarPositions = Array.from({ length: 12 }, () => {
+        return {
+          widthHeight: `${Math.min(
+            80,
+            Math.max(1, Math.floor(Math.random() * 8))
+          )}`,
+          top: `${Math.min(90, Math.max(10, Math.floor(Math.random() * 90)))}%`,
+          left: `${Math.min(70, Math.max(10, Math.floor(Math.random() * 70)))}%`,
+        };
+      });
+      setStarPositions(newStarPositions);
+    }
+  }, [count]);
 
   return (
     <S.MainContainer onClick={handleClick} $themechoice={isMoved.toString()}>
@@ -22,34 +44,20 @@ function Slider({ toggleTheme }: { toggleTheme: () => void }) {
       <S.Circle1 className={isMoved ? "sun-right" : "sun-left"}></S.Circle1>
       <S.Circle2 className={isMoved ? "sun-right" : "sun-left"}></S.Circle2>
       <S.Circle3 className={isMoved ? "sun-right" : "sun-left"}></S.Circle3>
-      {[...Array(12)].map((_, i) => {
-        const widthHeight = `${Math.min(
-          80,
-          Math.max(1, Math.floor(Math.random() * 8))
-        )}`;
-        const top = `${Math.min(
-          90,
-          Math.max(10, Math.floor(Math.random() * 90))
-        )}%`;
-        const left = `${Math.min(
-          70,
-          Math.max(10, Math.floor(Math.random() * 70))
-        )}%`;
-        return (
-          <S.Star
-            key={i}
-            className={isMoved ? "visible" : "invisible"}
-            src="star.svg"
-            alt="star"
-            style={{
-              width: `${widthHeight}px`,
-              height: `${widthHeight}px`,
-              top,
-              left,
-            }}
-          />
-        );
-      })}
+      {starPositions.map((star, i) => (
+        <S.Star
+          key={i}
+          className={isMoved ? "visible" : "invisible"}
+          src="star.svg"
+          alt="star"
+          style={{
+            width: `${star.widthHeight}px`,
+            height: `${star.widthHeight}px`,
+            top: star.top,
+            left: star.left,
+          }}
+        />
+      ))}
       {[...Array(8)].map((_, i) => {
         const index: number = [2, 5, 2, 5, 2, 5, 2, 5][i];
         const size: number = [80, 50, 50, 30, 20, 60, 80, 40][i];
@@ -68,14 +76,14 @@ function Slider({ toggleTheme }: { toggleTheme: () => void }) {
         return (
           <S.Cloud
             key={i}
-            className={isMoved ? "invisible" : "visibleCloud"}
             style={{
               zIndex: `${index}`,
               width: `${size}px`,
               height: `${size}px`,
-              bottom: `${bottom}px`,
+              bottom: `${bottom + (isMoved ? -80 : 0)}px`,
               right: `${right}px`,
               backgroundColor: `${color}`,
+              transition: "bottom 0.6s ease-in-out",
             }}
           />
         );
